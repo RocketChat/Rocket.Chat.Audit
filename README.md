4e85ada65c1ec49b3f5902d0e29ffd74329132f7
Cody A. Ray <cray@peak6.com>
2016-08-11 11:21:16 -0500

    Initial import

---
+++ README.md
@@ -0,0 +1,97 @@
+# Rocket.Chat.Audit
+
+Rocket.Chat.Audit is an application and library to inspect and record all
+Rocket.Chat communications as required by your Compliance Department.
+
+Out of the box, this application writes all audit files locally. It is expected
+that a batch process will regularly import them into downstream auditing systems.
+
+If you need real-time journaling or have other audit requirements, it should be
+quite simple to fork and build on this framework. Patches are welcome. :)
+
+## Configuration
+
+You can control how the oplog is tailed with the following common parameters:
+
+| Name            | Flag | Option           | Description                                             |
+| --------------- | ---- | ---------------- | ------------------------------------------------------- |
+| Master/Slave?   | `-m` | `--master-slave` | True if using master-slave oplog instead of replica set |
+
+
+There are three parameters for file-based auditing:
+
+| Name            | Flag | Option           | Description                                             |
+| --------------- | ---- | ---------------- | ------------------------------------------------------- |
+| Audit Directory | `-o` | `--output`       | Directory in which per-room audit logs should be stored |
+| File Store      | `-f` | `--file-store`   | Rocket.Chat File Upload file system path configuration  |
+| File Archive    | `-a` | `--file-archive` | Directory where File Uploads should be copied for archiving |
+
+## Installation
+
+An [ansible deployer](./deploy/) is in included for Ubuntu 14.04.
+The steps will be similar for other distributions.
+
+### Pre Installation
+
+1. Convert your Rocket.Chat MongoDB from a standalone [to a replica set]
+(https://docs.mongodb.org/manual/tutorial/convert-standalone-to-replica-set/).
+
+2. Configure your Rocket.Chat instance to store File Uploads on the File System.
+
+3. Disable "Keep Message History" in Rocket.Chat to avoid edited messages 
+   appearing in your audit logs multiple times.
+
+### Configuration
+
+Prior to install, its best to figure out a few things. The installation itself is opinionated
+out of the box but can configured however you want. The can all be configured in
+[`deploy/config.yml`](./deploy/config.yml)
+
+1. What user does Rocket.Chat run as? (`user` and `group`)
+
+2. Where does Rocket.Chat store File Uploads? (`file_store`)
+
+3. Where do you want the audit logs stored? (`audit_dir`)
+
+4. Where do you want the audit file archive stored? This could take a lot of space! (`file_archive`)
+
+### Deployment
+
+1. Add your host names in `inventory` for your different environments.
+
+2. Update the `remote_user` in `config.yaml` to SSH as this user.
+
+3. Make sure you have a public key on each host in your inventory for `remote_user`.
+
+4. Customize `config.yaml` for your installation.
+
+5. To deploy to a specific environment `env`, run
+
+   ansible-playbook -i inventory deploy.yaml -e env=development
+
+## Running
+
+Ansible installs Rocket.Chat.Audit as a service named `rocketchat.audit`.
+You can start, stop, or check that status with
+
+    sudo service rocketchat.audit start
+    sudo service rocketchat.audit stop
+    sudo service rocketchat.audit status
+
+## LICENSE
+
+Rocket.Chat.Audit is available under the Apache License, Version 2.0.
+
+    Copyright 2016 Peak6 Investments, L.P.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ rocketchat.audit.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+#
+# Rocket.Chat.Audit - rocketchat.audit.py
+#
+# Copyright 2016 Peak6 Investments, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Audits Rocket.Chat communications for compliance.
+
+Tails the MongoDB oplog for efficient real-time auditing.
+"""
+
+
+from abc import abstractmethod
+from abc import ABCMeta
+import argparse
+import logging
+import os.path
+import shutil
+import sys
+import time
+import traceback
+
+from cachetools import LRUCache
+import pymongo
+
+
+class Auditor(object):
+    """
+    Rocket.Chat auditor which tails the MongoDB oplog.
+
+    Passes each event to the appropriate AuditHandler callback.
+    """
+
+    def __init__(self, handler):
+        self.handler = handler
+        self.logger = logging.getLogger(self.__class__.__name__)
+        # this cache exists because only changed data is replicated in the oplog
+        # the room ID is never changed, so we have to map to the message ID
+        # the editedBy username isn't changed if you edit multiple times, so we need that too
+        self.cache = LRUCache(maxsize=10000)  # message _id => (room rid, editedBy username)
+
+    def tail_latest(self, oplog):
+        first = oplog.find().sort('$natural', pymongo.DESCENDING).limit(-10).next()
+        self.tail(oplog, first['ts'])
+
+    def tail(self, oplog, ts):
+        cursor = oplog.find({'ts': {'$gt': ts}}, oplog_replay=True,
+                            cursor_type=pymongo.cursor.CursorType.TAILABLE_AWAIT)
+        while cursor.alive:
+            for doc in cursor:
+                self._parse(doc)
+
+    def _parse(self, doc):
+        o = doc['o']
+        ns = doc['ns']
+        self.logger.debug("DOC %s", doc)
+        # insert a new message
+        if doc['op'] == 'i' and ns.endswith("rocketchat_message") and o.get('msg', False):
+            self.logger.info("INSERT %s", doc)
+            self.cache[o['_id']] = (o['rid'], None)  # so edits know the room for each message
+            self.handler.on_message(o['rid'], str(o['ts']), o['u']['username'], o['msg'])
+        # update an existing message
+        elif doc['op'] == 'u' and ns.endswith("rocketchat_message"):
+            self.logger.info("UPDATE %s", doc)
+            s = o['$set']
+            msg_id = doc['o2']['_id']
+            # cache doesn't persist across restarts, so we need to have a default
+            rid, edited_by = self.cache.get(msg_id, ('#unknown', 'unknown.user'))
+            edited_by = s['editedBy']['username'] if 'editedBy' in s else edited_by
+            self.cache[msg_id] = (rid, edited_by)  # so edits know the room for each message
+            self.handler.on_message(rid, str(s['editedAt']), edited_by, s['msg'])
+        # upload a file attachment
+        elif ns.endswith("rocketchat_message") and "attachments" in o:
+            self.logger.info("FILE", doc)
+            title = o['attachments'][0]['title']
+            self.handler.on_file(o['rid'], str(o['ts']), o['u']['username'], title,
+                                 o['file']['_id'], o['attachments'][0]['image_type'])
+
+
+class AuditHandler(object):
+    """
+    Base class for AuditHandlers which define callbacks for different Rocket.Chat events.
+    """
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def on_message(self, room_id, ts, username, msg):
+        pass
+
+    @abstractmethod
+    def on_file(self, room_id, ts, username, title, file_id, image_type):
+        pass
+
+
+class FileAuditHandler(AuditHandler):
+    """
+    AuditHandler which writes one file per chat room (channel, private group, or direct message),
+    journals all file uploads to a special file, and copies all files to an archive directory.
+    """
+
+    def __init__(self, audit_dir, file_store, file_archive, file_journal='file_uploads'):
+        """
+        :param audit_dir: directory in which to store per-room audit logs
+        :param file_store: Rocket.Chat File Upload file system path configuration
+        :param file_archive: directory to which File Uploads should be copied for archiving
+        :param file_journal: file to which to record File Uploads
+        """
+        self.audit_dir = audit_dir
+        self.file_store = file_store
+        self.file_archive = file_archive
+        self.file_journal = file_journal
+
+    def on_message(self, room_id, ts, username, msg):
+        self._log(room_id, ts, username, msg)
+
+    def on_file(self, room_id, ts, username, title, file_id, image_type):
+        ext = image_type.split("/")[1]
+        filename = "%s.%s" % (file_id, ext)
+        message = "%s %s" % (title, filename)
+        # log file uploads both to the room and the file_journal
+        self._log(room_id, ts, username, message)
+        self._log(self.file_journal, ts, username, message)
+        # copy the file from Rocket.Chat's file store to the file archive
+        shutil.copy(self.file_store + filename, self.file_archive + filename)
+
+    def _log(self, filename, ts, username, msg):
+        with open("%s%s" % (self.audit_dir, filename), 'a') as target:
+            target.write("%s %s: %s\n" % (ts, username, msg))
+
+
+def main(audit_dir, file_store, file_archive, is_master_slave):
+    print {"audit_dir": audit_dir, "file_store": file_store, "file_archive": file_archive}
+    handler = FileAuditHandler(audit_dir, file_store, file_archive)
+    auditor = Auditor(handler)
+
+    c = pymongo.MongoClient()
+    oplog = c.local.oplog['$main'] if is_master_slave else c.local.oplog.rs
+
+    while True:
+        try:
+            auditor.tail_latest(oplog)
+            time.sleep(1)
+        except Exception, e:
+            traceback.print_exc(e)
+
+
+# MUST match your Rocket.Chat configuration for File Upload file system path
+ROCKETCHAT_FILE_UPLOAD_DIRECTORY = "/var/lib/rocketchat.filestore/"
+
+AUDIT_FILE_UPLOAD_DIRECTORY = "/var/lib/rocketchat.audit/filearchive/"
+AUDIT_CHAT_DIRECTORY = "/var/lib/rocketchat.audit/chats/"
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Audit Rocket.Chat Communications')
+    parser.add_argument('-o', '--output', dest='audit_dir', default=AUDIT_CHAT_DIRECTORY,
+                        help='Directory in which per-room audit logs should be stored')
+    parser.add_argument('-f', '--file-store', default=ROCKETCHAT_FILE_UPLOAD_DIRECTORY,
+                        help='Rocket.Chat File Upload file system path configuration')
+    parser.add_argument('-a', '--file-archive', default=AUDIT_FILE_UPLOAD_DIRECTORY,
+                        help='Directory where File Uploads should be copied for archiving')
+    parser.add_argument('-m', '--master-slave', action='store_true',
+                        help='True if using master-slave oplog instead of replica set')
+    parser.add_argument('-v', '--verbose', action='count', help='verbose output')
+    args = parser.parse_args()
+
+    log_format = '%(asctime)s %(levelname)s: %(message)s'
+    level = [logging.WARNING, logging.INFO, logging.DEBUG][args.verbose or 0]
+    logging.basicConfig(level=level, format=log_format, stream=sys.stderr)
+    norm = lambda d: os.path.join(d, '')  # normalize directory so that it ends with a slash
+    main(norm(args.audit_dir), norm(args.file_store), norm(args.file_archive), args.master_slave)
